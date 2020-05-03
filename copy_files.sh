e6a071455836fbeb7e39471944290b07bcfc447b
Helge Klein <3407928+helgeklein@users.noreply.github.com>
2020-05-04 00:41:32 +0200

    Added ".ssh" to the delete ignore list

--- copy_files.sh
+++ copy_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove all previously copied files and folders (excluding metadata) in the current (backup) directory
-find /backup/data -maxdepth 1 -mindepth 1 -not -iname ".git" | xargs --no-run-if-empty rm -r
+find /backup/data -maxdepth 1 -mindepth 1 -not -iname ".git" -not -iname ".ssh" | xargs --no-run-if-empty rm -r
 
 # Copy the files and directories on the list to the current (backup) directory
 # xargs does not start a shell, so we need to do it ourselves or globbing won't be available
